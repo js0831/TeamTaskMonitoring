afc9b6dc5e6230f357cf4b58f27babdff6ca30e2
81254807 <81254807@corp.root>
2019-07-29 09:36:27 +0800

    add error notification on error handler

--- src/app/shared/interceptors/error-handler.service.ts
+++ src/app/shared/interceptors/error-handler.service.ts
@@ -1,26 +1,39 @@
-import { ErrorHandler, Injectable} from '@angular/core';
+import { ErrorHandler, Injectable, Injector} from '@angular/core';
 import {
     HttpErrorResponse
 } from '@angular/common/http';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Injectable()
 export class ErrorsHandler implements ErrorHandler {
 
+    public message: NzMessageService;
+    constructor(
+        private injector: Injector
+    ) {
+        this.message = this.message || this.injector.get(NzMessageService);
+    }
     handleError(error: Error) {
         // Do whatever you like with the error (send it to the server?)
         // And log it to the console
         // console.error('It happens: ', error);
-        if (error instanceof HttpErrorResponse) {
-        if (!navigator.onLine) {
-            console.log('no internet');
-        } else {
-            console.log(error.status);
-            console.log(error.message);
-        }
 
+        let message;
+        if (error instanceof HttpErrorResponse) {
+            if (!navigator.onLine) {
+                console.log('no internet');
+                message = 'No Internet Connection';
+            } else {
+                console.log(error.status);
+                console.log(error.message);
+                message = `${error.status} ${error.message}`;
+            }
         } else {
             console.log(error);
             console.log('CLIENT ERROR');
+            message = 'Client error';
         }
+
+        this.message.create('error', message);
     }
 }
